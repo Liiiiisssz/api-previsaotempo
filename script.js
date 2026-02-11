aec5c8da74fb1da6238a59fc36e8ecf9639ab276
Lis <elis_jasper@estudante.sesisenai.org.br>
2026-02-11 18:59:07 -0300

    finalização

--- script.js
+++ script.js
@@ -70,28 +70,61 @@ input.addEventListener('keyup', function(event){
     }
 });
 
+document.addEventListener("DOMContentLoaded", () =>{
+    previsaoJaragua();
+})
+
+async function previsaoJaragua(){
+    datas();
+    try{
+        const dados = await fetch(
+            `https://api.open-meteo.com/v1/forecast?latitude=-26.4861&longitude=-49.0667&current_weather=true&hourly=temperature_2m,relative_humidity_2m,windspeed_10m,weathercode&daily=weathercode,temperature_2m_max,temperature_2m_min&forecast_days=4&timezone=auto`
+        )
+        const climaData = await dados.json();
+            
+        const hora = climaData.current_weather.is_day;
+        const temp = climaData.current_weather.temperature;
+        const vento = climaData.current_weather.windspeed;
+        const umidade = climaData.hourly.relative_humidity_2m[0];
+        const codTempo = climaData.current_weather.weathercode;
+
+        const codTempoD1 = climaData.daily.weathercode[1];
+        const codTempoD2 = climaData.daily.weathercode[2];
+        const codTempoD3 = climaData.daily.weathercode[3];
+
+        const tempD1Max = climaData.daily.temperature_2m_max[1];
+        const tempD1Min = climaData.daily.temperature_2m_min[1];
+        const tempD2Max = climaData.daily.temperature_2m_max[2];
+        const tempD2Min = climaData.daily.temperature_2m_min[2];
+        const tempD3Max = climaData.daily.temperature_2m_max[3];
+        const tempD3Min = climaData.daily.temperature_2m_min[3];
+
+        trocarFundo(hora);
+        textoEicons(hora, codTempo, codTempoD1, codTempoD2, codTempoD3);
+        
+        document.getElementById("temperatura").innerHTML = `${temp}°C`;
+        document.getElementById("velocidade-vento").innerHTML = `${vento} km/h`;
+        document.getElementById("umidade").innerHTML = `${umidade}%`;
+        document.getElementById("tempo").innerHTML = weatherMap[codTempo];
+
+        document.getElementById("temperatura-dia-um-max").innerHTML = `${tempD1Max}°C`
+        document.getElementById("temperatura-dia-um-min").innerHTML = `${tempD1Min}°C`
+        document.getElementById("temperatura-dia-dois-max").innerHTML = `${tempD2Max}°C`
+        document.getElementById("temperatura-dia-dois-min").innerHTML = `${tempD2Min}°C`
+        document.getElementById("temperatura-dia-tres-max").innerHTML = `${tempD3Max}°C`
+        document.getElementById("temperatura-dia-tres-min").innerHTML = `${tempD3Min}°C`
+
+    } catch(erro){
+        document.getElementById("resultado").innerText = "Cidade não encontrada";
+    }
+}
+
 async function buscarClima(){
     const cidade = document.getElementById("cidade").value;
-    
     if(!cidade) return;
 
-    const data = new Date();
-    const nomeDia = diasSemana[data.getDay()]
-
-    const hoje = data.getDay();
-    const nomeDia1 = diasSemanaAbv[(hoje + 1) % 7];
-    const nomeDia2 = diasSemanaAbv[(hoje + 2) % 7];
-    const nomeDia3 = diasSemanaAbv[(hoje + 3) % 7];
-
-    const dia = String(data.getDate()).padStart(2, "0");
-    const mes = data.toLocaleString('pt-BR', {month: 'long'});
-
+    datas();
     try{
-        document.getElementById("data").innerHTML = `${nomeDia}, ${dia} de ${mes.charAt(0).toLocaleUpperCase() + mes.slice(1)}`
-        document.getElementById("nome-dia-um").innerHTML = nomeDia1;
-        document.getElementById("nome-dia-dois").innerHTML = nomeDia2;
-        document.getElementById("nome-dia-tres").innerHTML = nomeDia3;
-
         const geoResp = await fetch(
             `https://geocoding-api.open-meteo.com/v1/search?name=${cidade}&language=pt`
         );
@@ -123,115 +156,14 @@ async function buscarClima(){
         const tempD3Max = climaData.daily.temperature_2m_max[3];
         const tempD3Min = climaData.daily.temperature_2m_min[3];
 
-        if(!hora){
-            const img = document
-        }
-
-        switch(codTempo){
-            case 0: case 1:
-                iconeTempo = hora ? "assets/sol-grande.png" : "assets/lua.png";
-                frase = hora ? frasesDia.sol : frasesNoite.sol;
-                break;
-            case 2:
-                iconeTempo = hora ? "assets/dia-nublado-grande.png" : "assets/noite-nublada.png";
-                frase = hora ? frasesDia.parcialmenteNublado : frasesNoite.parcialmenteNublado;
-                break;
-            case 3: case 45: case 48:
-                iconeTempo = "assets/nublado.png";
-                frase = hora ? frasesDia.nublado : frasesNoite.nublado;
-                break;
-            case 51: case 53: case 55: case 56: case 57: case 61: case 63:
-            case 65: case 66: case 67: case 80: case 81: case 82: 
-                iconeTempo = "assets/chuva-grande.png";
-                frase = hora ? frasesDia.chuva : frasesNoite.chuva;
-                break;
-            case 71: case 73: case 75: case 77: case 85: case 86:
-                iconeTempo = "assets/neve-grande.png";
-                frase = hora ? frasesDia.neve : frasesNoite.neve;
-                break;
-            case 95: case 96: case 99:
-                iconeTempo = "assets/tempestade-grande.png";
-                frase = hora ? frasesDia.tempestade : frasesNoite.tempestade;
-                break;
-        }
-
-        switch(codTempoD1){
-            case 0: case 1:
-                iconeTempoD1 = "assets/sol.png";
-                break;
-            case 2:
-                iconeTempoD1 ="assets/dia-nublado.png";
-                break;
-            case 3: case 45: case 48:
-                iconeTempoD1 = "assets/nublado-menor.png";
-                break;
-            case 51: case 53: case 55: case 56: case 57: case 61: case 63:
-            case 65: case 66: case 67: case 80: case 81: case 82: 
-                iconeTempoD1 = "assets/chuva.png";
-                break;
-            case 71: case 73: case 75: case 77: case 85: case 86:
-                iconeTempoD1 = "assets/neve.png";
-                break;
-            case 95: case 96: case 99:
-                iconeTempoD1 = "assets/tempestade.png";
-                break;
-        }
-
-        switch(codTempoD2){
-            case 0: case 1:
-                iconeTempoD2 = "assets/sol.png";
-                break;
-            case 2:
-                iconeTempoD2 ="assets/dia-nublado.png";
-                break;
-            case 3: case 45: case 48:
-                iconeTempoD2 = "assets/nublado-menor.png";
-                break;
-            case 51: case 53: case 55: case 56: case 57: case 61: case 63:
-            case 65: case 66: case 67: case 80: case 81: case 82: 
-                iconeTempoD2 = "assets/chuva.png";
-                break;
-            case 71: case 73: case 75: case 77: case 85: case 86:
-                iconeTempoD2 = "assets/neve.png";
-                break;
-            case 95: case 96: case 99:
-                iconeTempoD2 = "assets/tempestade.png";
-                break;
-        }
-
-        switch(codTempoD3){
-            case 0: case 1:
-                iconeTempoD3 = "assets/sol.png";
-                break;
-            case 2:
-                iconeTempoD3 ="assets/dia-nublado.png";
-                break;
-            case 3: case 45: case 48:
-                iconeTempoD3 = "assets/nublado-menor.png";
-                break;
-            case 51: case 53: case 55: case 56: case 57: case 61: case 63:
-            case 65: case 66: case 67: case 80: case 81: case 82: 
-                iconeTempoD3 = "assets/chuva.png";
-                break;
-            case 71: case 73: case 75: case 77: case 85: case 86:
-                iconeTempoD3 = "assets/neve.png";
-                break;
-            case 95: case 96: case 99:
-                iconeTempoD3 = "assets/tempestade.png";
-                break;
-        }
-
+        trocarFundo(hora);
+        textoEicons(hora, codTempo, codTempoD1, codTempoD2, codTempoD3);
+        
         document.getElementById("nome-cidade").innerHTML = cidadeOficial;
         document.getElementById("temperatura").innerHTML = `${temp}°C`;
         document.getElementById("velocidade-vento").innerHTML = `${vento} km/h`;
         document.getElementById("umidade").innerHTML = `${umidade}%`;
         document.getElementById("tempo").innerHTML = weatherMap[codTempo];
-        document.getElementById("icone-temp").src = iconeTempo;
-        document.getElementById("frase").innerHTML = frase;
-
-        document.getElementById("icone-temp-dia-um").src = iconeTempoD1;
-        document.getElementById("icone-temp-dia-dois").src = iconeTempoD2;
-        document.getElementById("icone-temp-dia-tres").src = iconeTempoD3;
 
         document.getElementById("temperatura-dia-um-max").innerHTML = `${tempD1Max}°C`
         document.getElementById("temperatura-dia-um-min").innerHTML = `${tempD1Min}°C`
@@ -242,5 +174,154 @@ async function buscarClima(){
 
     } catch(erro){
         document.getElementById("resultado").innerText = "Cidade não encontrada";
+    }
+}
+
+function datas(){
+    const data = new Date();
+    const nomeDia = diasSemana[data.getDay()]
+
+    const hoje = data.getDay();
+    const nomeDia1 = diasSemanaAbv[(hoje + 1) % 7];
+    const nomeDia2 = diasSemanaAbv[(hoje + 2) % 7];
+    const nomeDia3 = diasSemanaAbv[(hoje + 3) % 7];
+
+    const dia = String(data.getDate()).padStart(2, "0");
+    const mes = data.toLocaleString('pt-BR', {month: 'long'});
+
+    document.getElementById("data").innerHTML = `${nomeDia}, ${dia} de ${mes.charAt(0).toLocaleUpperCase() + mes.slice(1)}`
+    document.getElementById("nome-dia-um").innerHTML = nomeDia1;
+    document.getElementById("nome-dia-dois").innerHTML = nomeDia2;
+    document.getElementById("nome-dia-tres").innerHTML = nomeDia3;
+}
+
+function textoEicons(hora, codTempo, codTempoD1, codTempoD2, codTempoD3){
+    switch(codTempo){
+        case 0: case 1:
+            iconeTempo = hora ? "assets/sol-grande.png" : "assets/lua.png";
+            frase = hora ? frasesDia.sol : frasesNoite.sol;
+            break;
+        case 2:
+            iconeTempo = hora ? "assets/dia-nublado-grande.png" : "assets/noite-nublada.png";
+            frase = hora ? frasesDia.parcialmenteNublado : frasesNoite.parcialmenteNublado;
+            break;
+        case 3: case 45: case 48:
+            iconeTempo = "assets/nublado.png";
+            frase = hora ? frasesDia.nublado : frasesNoite.nublado;
+            break;
+        case 51: case 53: case 55: case 56: case 57: case 61: case 63:
+        case 65: case 66: case 67: case 80: case 81: case 82: 
+            iconeTempo = "assets/chuva-grande.png";
+            frase = hora ? frasesDia.chuva : frasesNoite.chuva;
+            break;
+        case 71: case 73: case 75: case 77: case 85: case 86:
+            iconeTempo = "assets/neve-grande.png";
+            frase = hora ? frasesDia.neve : frasesNoite.neve;
+            break;
+        case 95: case 96: case 99:
+            iconeTempo = "assets/tempestade-grande.png";
+            frase = hora ? frasesDia.tempestade : frasesNoite.tempestade;
+            break;
+    }
+
+    switch(codTempoD1){
+        case 0: case 1:
+            iconeTempoD1 = "assets/sol.png";
+            break;
+        case 2:
+            iconeTempoD1 ="assets/dia-nublado.png";
+            break;
+        case 3: case 45: case 48:
+            iconeTempoD1 = "assets/nublado-menor.png";
+            break;
+        case 51: case 53: case 55: case 56: case 57: case 61: case 63:
+        case 65: case 66: case 67: case 80: case 81: case 82: 
+            iconeTempoD1 = "assets/chuva.png";
+            break;
+        case 71: case 73: case 75: case 77: case 85: case 86:
+            iconeTempoD1 = "assets/neve.png";
+            break;
+        case 95: case 96: case 99:
+            iconeTempoD1 = "assets/tempestade.png";
+            break;
+    }
+
+    switch(codTempoD2){
+        case 0: case 1:
+            iconeTempoD2 = "assets/sol.png";
+            break;
+        case 2:
+            iconeTempoD2 ="assets/dia-nublado.png";
+            break;
+        case 3: case 45: case 48:
+            iconeTempoD2 = "assets/nublado-menor.png";
+            break;
+        case 51: case 53: case 55: case 56: case 57: case 61: case 63:
+        case 65: case 66: case 67: case 80: case 81: case 82: 
+            iconeTempoD2 = "assets/chuva.png";
+            break;
+        case 71: case 73: case 75: case 77: case 85: case 86:
+            iconeTempoD2 = "assets/neve.png";
+            break;
+        case 95: case 96: case 99:
+            iconeTempoD2 = "assets/tempestade.png";
+            break;
+    }
+
+    switch(codTempoD3){
+        case 0: case 1:
+            iconeTempoD3 = "assets/sol.png";
+            break;
+        case 2:
+            iconeTempoD3 ="assets/dia-nublado.png";
+            break;
+        case 3: case 45: case 48:
+            iconeTempoD3 = "assets/nublado-menor.png";
+            break;
+        case 51: case 53: case 55: case 56: case 57: case 61: case 63:
+        case 65: case 66: case 67: case 80: case 81: case 82: 
+            iconeTempoD3 = "assets/chuva.png";
+            break;
+        case 71: case 73: case 75: case 77: case 85: case 86:
+            iconeTempoD3 = "assets/neve.png";
+            break;
+        case 95: case 96: case 99:
+            iconeTempoD3 = "assets/tempestade.png";
+            break;
+    }
+
+    document.getElementById("icone-temp").src = iconeTempo;
+    document.getElementById("frase").innerHTML = frase;
+
+    document.getElementById("icone-temp-dia-um").src = iconeTempoD1;
+    document.getElementById("icone-temp-dia-dois").src = iconeTempoD2;
+    document.getElementById("icone-temp-dia-tres").src = iconeTempoD3;
+}
+
+function trocarFundo(hora){
+    const img = document.getElementById("body");
+    const pesquisa = document.getElementById("pesquisa");
+    const principal = document.getElementById("bg-principal");
+    const botao = document.getElementById("botao");
+    const um = document.getElementById("dia-um");
+    const dois = document.getElementById("dia-dois");
+    const tres = document.getElementById("dia-tres");
+
+    if(!hora){
+        img.classList.replace("bg-[url(/assets/ceu.jpg)]", "bg-[url(/assets/noite.png)]");
+        pesquisa.classList.replace("bg-sky-200/50", "bg-slate-600/50");
+        principal.classList.replace("bg-sky-200/50", "bg-slate-600/50");
+        botao.classList.replace("bg-sky-400", "bg-slate-600");
+        um.classList.replace("bg-sky-300/50", "bg-slate-500/50");
+        dois.classList.replace("bg-sky-300/50", "bg-slate-500/50");
+        tres.classList.replace("bg-sky-300/50", "bg-slate-500/50");
+    } else {
+        img.classList.replace("bg-[url(/assets/noite.png)]", "bg-[url(/assets/ceu.jpg)]");
+        pesquisa.classList.replace("bg-slate-600/50", "bg-sky-200/50");
+        principal.classList.replace("bg-slate-600/50", "bg-sky-200/50");
+        botao.classList.replace("bg-slate-600", "bg-sky-400");
+        um.classList.replace("bg-slate-500/50", "bg-sky-300/50");
+        dois.classList.replace("bg-slate-500/50", "bg-sky-300/50");
+        tres.classList.replace("bg-slate-500/50", "bg-sky-300/50");
     }
 }
